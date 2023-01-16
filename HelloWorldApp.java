a48c0be34d748bdc7a365aa9d7dc9345a81d7b8e
Draggard11 <Draggard2003@gmail.com>
2023-01-16 12:27:12 +0100

    no notes

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,12 +1,3 @@
-/**
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
- */
-/**
- * My first project
- * It says "Hello World!"
- * Hello World!
- */
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
